16c0fb571e12029afbc46c0aef7cc73c4048d953
Matt Brophy <matt@brophy.org>
2022-08-16 16:15:26 -0400

    defensive route.module

--- packages/remix-server-runtime/server.ts
+++ packages/remix-server-runtime/server.ts
@@ -53,7 +53,7 @@ export const createRequestHandler: CreateRequestHandlerFunction = (
         handleDataRequest: build.entry.module.handleDataRequest,
         serverMode,
       });
-    } else if (matches && !matches[matches.length - 1].route.module.default) {
+    } else if (matches && !matches[matches.length - 1].route.module?.default) {
       response = await handleResourceRequest({
         request,
         loadContext,
@@ -618,7 +618,7 @@ function getMatchesUpToDeepestBoundary(
   let deepestBoundaryIndex: number = -1;
 
   matches.forEach((match, index) => {
-    if (match.route.module[key]) {
+    if (match.route.module?.[key]) {
       deepestBoundaryIndex = index;
     }
   });
